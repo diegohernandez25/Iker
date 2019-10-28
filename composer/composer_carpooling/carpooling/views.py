3806393cb107a9294f3bd457a87d5724020f261d
Rodrigo <rodrigo.pereira@ua.pt>
2019-10-28 22:05:22 +0000

    composer:delineamento de um get trips

--- composer/composer_carpooling/carpooling/views.py
+++ composer/composer_carpooling/carpooling/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from rest_framework.views import APIView
 import json
+import requests
+
+
+ipgfEndpoint = ""
+reviewEndpoint = ""
+reservasEndpoint = ""
+paymentEndpoint = ""
+
 
 def home(request):
     return HttpResponse("Hello, there!")
+
+
+#FIXME Nenhum endpoint está funcional
 
 #Este pedido vai retornar uma lista de objetos trip
 #(Cujo o formato está por definir, tendo em conta as necessidades do frontend)
@@ -16,4 +27,20 @@ def home(request):
 #5. Devolver lista de trips com info necessaria para a aplicação móvel
 class getTrips(APIView):
     def get(self, request):
-        return JsonResponse({'foo':'bar'})
+        #Obter a lista de todas as trips que cuprem os requisitos de partida e chegada na query
+        getTripsReq = requests.get(url = ipgfEndpoint+"/getTrips", params = {'init_coords':request.query_params.get('init_coords'), 
+            'final_coords':request.query_params.get('final_coords'),
+            'init_time':request.query_params.get('init_time') })
+        listOfTripIDS = getTripsReq.json()
+
+        #filtrar todos os ids de trips que teem lugares disponiveis
+        listOfTripIDS=list(filter(lambda id: requests.get(url = reservasEndpoint+"/getAvailableElements"+"/"+id)['availableSeats']>=request.query_params.get('seats'), listOfTripIDS))
+        
+        responseJSON='{}'
+        #Obter informacao acerca das trips
+        for tripID in listOfTripIDS:
+            tripInfo=request.get(url=ipgfEndpoint+"/tripDetails"+"/"+tripID).json()
+            rating=requests.get(url=reviewEndpoint+"/object"+"/"+tripInfo['driver'])
+            #Construir reponseJSON
+
+        return JsonResponse(responseJSON)
