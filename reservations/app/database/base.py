fc54e324a85ca045998af4365a01d37f8f59a940
Diego Hernandez <diego.hernandez.c25@gmail.com>
2019-11-19 23:23:46 +0000

    base.py altered on reservtion and composer

--- reservations/app/database/base.py
+++ reservations/app/database/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql://root:root@127.0.0.1:3306/booking", echo=False)
+engine = create_engine("mysql://root:root@127.0.0.1:32000/booking", echo=False)
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
