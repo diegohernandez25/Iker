9cd350cd177f1e2fee90d45897debee2c8b6c806
Diego Hernandez <diego.hernandez.c25@gmail.com>
2019-11-19 22:03:08 +0000

    Created new composer project

---
+++ composer/app/app.py
@@ -0,0 +1,48 @@
+from typing import List, Dict
+from flask import Flask, redirect, url_for, request, jsonify
+import mysql.connector
+import json
+import datetime
+import json
+
+from logging.config import dictConfig
+from sqlalchemy import create_engine
+from sqlalchemy import Column, String, Integer, Date, Float, ForeignKey, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+from database.operations import *
+from database.base import Base, engine, Session
+from json_op import *
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+app = Flask(__name__)
+
+session = Session()
+
+@app.route('/')
+def index() -> str:
+    return "Booking"
+
+@app.route('/create_usr')
+def create_user():
+    #Get the authentication id of the user obtained by the authentication service
+    auth_id = request.args.get('id_auth')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ composer/app/database/entities.py
@@ -0,0 +1,83 @@
+from sqlalchemy import Column, String, Integer, Date, Float, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from database.base import Base, engine, Session
+from datetime import date
+import time
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id                      = Column(Integer, primary_key=True)
+    id_authentication       = Column(Integer, unique=True, nullable=False)
+    id_owner_booking        = Column(Integer, unique=True, nullable=False)
+    id_client_booking       = Column(Integer, unique=True, nullable=False)
+    access_token            = Column(String, unique=True, nullable=False)
+
+    trip        = relationship("trip", backref="user")
+    reservation = relationship("reservation", backref="user")
+
+
+    def __str__(self):
+        return "         id: %s\n" % (str(self.id)) +\
+            "id_authentication: %s\n" % (self.id_authentication) +\
+            " id_owner_booking: %s\n" % (self.id_owner_booking) +\
+            "id_client_booking: %s\n" % (self.id_client_booking) +\
+            "     access_token: %s\n" % (self.access_token)
+
+    def get_dict(self):
+        return {
+            "id"                : self.id,
+            "id_authentication" : self.id_authentication,
+            "id_owner_booking"  : self.id_owner_booking,
+            "id_client_booking" : self.id_client_booking,
+            "access_token"      : self.access_token
+        }
+
+
+class Trip(Base):
+
+    __tablename__ = 'trip'
+
+    id                  = Column(Integer, primary_key=True)
+    id_domain_booking   = Column(Integer, unique=True)
+    id_iptf             = Column(Integer, unique=True)
+    id_user             = Column(Integer, ForeignKey('user.id'))
+
+    def __str__(self):
+        return "         id: %s\n" % (str(self.id)) +\
+            "id_domain_booking: %s\n" % (self.id_domain_booking) +\
+            "          id_iptf: %s\n" % (self.id_iptf) +\
+            "          id_user: %s\n" % (self.id_user)
+
+    def get_dict(self):
+        return {
+            "id"                : self.id,
+            "id_domain_booking" : self.id_domain_booking ,
+            "id_iptf"  : self.id_iptf,
+            "id_user" : self.id_user
+        }
+
+
+class Reservation(Base):
+
+    __tablename__ = 'reservation'
+
+    id                  = Column(Integer, primary_key=True)
+    id_element_booking  = Column(Integer, unique=True)
+    id_iptf             = Column(Integer, unique=True)
+    id_user             = Column(Integer, ForeignKey('user.id'))
+
+    def __str__(self):
+        return "         id: %s\n" % (str(self.id)) +\
+            "id_element_booking:%s\n" % (self.id_domain_booking) +\
+            "          id_iptf: %s\n" % (self.id_iptf) +\
+            "          id_user: %s\n" % (self.id_user)
+
+    def get_dict(self):
+        return {
+            "id"                : self.id,
+            "id_domain_booking" : self.id_domain_booking ,
+            "id_iptf"  : self.id_iptf,
+            "id_user" : self.id_user
+        }
