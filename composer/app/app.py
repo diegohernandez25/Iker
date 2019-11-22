ca6dbf4d443a28620bcbb3d00eedd04418274ce2
Diego Hernandez <diego.hernandez.c25@gmail.com>
2019-11-22 17:42:45 +0000

    Upddated Composer. Updated Databases tables and operations. Added Register Trip Flux & User Creation.

--- composer/app/app.py
+++ composer/app/app.py
@@ -1,10 +1,15 @@
 from typing import List, Dict
-from flask import Flask, redirect, url_for, request, jsonify
+from flask import Flask, redirect, url_for, request, jsonify, request
 import mysql.connector
 import json
 import requests
 import datetime
 import json
+import requests
+import string
+import random
+import logging
+import sys
 
 from logging.config import dictConfig
 from sqlalchemy import create_engine
@@ -14,6 +19,13 @@ from sqlalchemy.orm import relationship, sessionmaker
 
 from database.operations import *
 from database.base import Base, engine, Session
+from database.entities import *
+
+
+URL_RESERVATION = "http://localhost:5002/1/"
+URL_TRIP_FOLLOWER = "http://localhost:8081/"
+
+logging.basicConfig(level=logging.DEBUG)
 
 dictConfig({
     'version': 1,
@@ -39,15 +51,6 @@ session = Session()
 def index() -> str:
     return "Booking"
 
-@app.route('/create_usr')
-def create_user():
-    #Get the authentication id of the user obtained by the authentication service
-    auth_id = request.args.get('id_auth')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
-
 @app.route("/test_trip", methods=['POST'])
 def test_trip():
 	print(requests.post("localhost:8081/probe_trip",data=request.json))
@@ -56,4 +59,75 @@ def test_trip():
 def put_trip():
 	requests.post("localhost:8081/register_trip",data=request.json)
 
+"""
+Account Setup Flow
+"""
+#TODO: Check if it is not registered. DONE
+@app.route("/create_usr", methods=['POST'])
+def createUser():
+    #Get Authentication Id from query parameter
+    authentication_id = request.args.get('usr_id')
 
+    #Create Owner in booking service
+    body        = request.json
+    if set(["name", "information"]).issubset(set(body.keys())) and\
+        not usr_exists(session, authentication_id):
+        #Get new owner id
+        b_json  = {"name":body["name"],"information":body["information"]}
+        r       = requests.post(URL_RESERVATION + "owner", json=b_json)
+
+        owner_id = r.json()['id']
+
+        #Get new client id
+        b_json  = {"name":body["name"],"information":body["information"]}
+        r       = requests.post(URL_RESERVATION + "client", json=b_json)
+        client_id = r.json()['id']
+
+        #generate random access_token
+        access_token = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(15))
+
+        #create line register to the composer database
+        usr = create_user(session, authentication_id, owner_id, client_id,
+                            access_token)
+
+        response = {"id": usr.id, "access_token":usr.access_token}
+        return json.dumps(response)
+
+    else:
+        app.logger.error('Invalid JSON body. "name" or "information" fields may\
+            not be explicit.')
+        return "ERROR"
+
+@app.route("/register_trip", methods=['POST'])
+def book_trip()->str:
+
+    user_id         = request.args.get('usr_id')
+    access_token    = request.args.get('access_token')
+    body            = request.json
+
+    if set(["StartCoords", "EndCoords","Consumption","AvoidTolls","StartTime",
+            "EndTime","MaxDetour","FuelType"]).issubset(set(body.keys())) and\
+            valid_usr(session, user_id, access_token):
+
+        r       = requests.post(URL_TRIP_FOLLOWER + "register_trip", json=body)
+        id_iptf = r.json()
+
+        user    = get_usr(session, user_id)
+        owner_id    = user.id_owner_booking
+        url     = URL_RESERVATION+"owner/"+str(owner_id)+"/domain"
+        d_json  = {"name": body["name"], "information": body["information"]}
+        r       = requests.post(url, json=d_json)
+        id_domain_booking   = r.json()['id']
+
+        #Save trip mapping
+        trip = create_trip(session, id_domain_booking, id_iptf, user_id)
+        return json.dumps(trip.get_dict())
+
+    else:
+        return "ERROR"
+
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    app.run(host='0.0.0.0')
