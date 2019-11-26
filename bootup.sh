88e9c964b7877590796812f70bd76cba2b2837ae
André Pinho <andre.s.pinho@ua.pt>
2019-11-26 01:19:23 +0000

    Fix on script

--- bootup.sh
+++ bootup.sh
@@ -1,14 +1,14 @@
 echo "Starting Review Service"
-(cd reviewService && docker-composer up -d)
+(cd reviewService && docker-compose up -d)
 
 echo "Starting Reservation Service"
-(cd reservations && docker-composer up -d)
+(cd reservations && docker-compose up -d)
 
 echo "Starting Payment Service"
 (cd payment && make build && make mount_detach)
 
 echo "Starting IPTF Service"
-(cd iptf && mvn package && docker-composer up -d)
+(cd iptf && mvn package && docker-compose up -d)
 
 echo "Starting Composer Service"
-(cd composer && docker-composer up -d)
+(cd composer && docker-compose up -d)
