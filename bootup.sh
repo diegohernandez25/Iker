4af63ec191e9a2c087124813e3a171f8ca683d22
André Pinho <andre.s.pinho@ua.pt>
2019-11-26 01:32:48 +0000

    Fix on script, added script to stop all

--- bootup.sh
+++ bootup.sh
@@ -8,7 +8,7 @@ echo "Starting Payment Service"
 (cd payment && make build && make mount_detach)
 
 echo "Starting IPTF Service"
-(cd iptf && mvn package && docker-compose up -d)
+(cd iptf && mvn -Dmaven.test.skip=true package && docker-compose up -d)
 
 echo "Starting Composer Service"
 (cd composer && docker-compose up -d)
