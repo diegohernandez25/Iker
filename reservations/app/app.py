08bd33cb3ea0e399d9ff6184449481f80689cf6a
Diego Hernandez <diego.hernandez.c25@gmail.com>
2019-12-03 15:09:29 +0000

    Updated Reservations. Fixed DB Sessions.

--- reservations/app/app.py
+++ reservations/app/app.py
@@ -33,8 +33,6 @@ dictConfig({
 
 app = Flask(__name__)
 
-session = Session()
-
 @app.route('/')
 def index() -> str:
     return "Booking"
@@ -43,21 +41,29 @@ def index() -> str:
 @app.route('/service', methods=['POST'])
 def create_service_api()->str:
 
+    session = Session()
     body = request.json
     service = create_service(session, body['name'], body['information'],None,datetime.datetime.now())
     service.url="/service/%d" % (service.id)
     session.commit()
 
-    return json.dumps(service.get_dict())
+    response = service.get_dict()
+    session.close()
+
+    return json.dumps(response)
 
 
 @app.route('/<id>', methods=['GET'])
-def get_service_api(id)->str():
+def get_service_api(id):
 
+    session = Session()
     service = get_service(session, id)
     if service is not None:
-        return get_json_service(service)
+        response = get_json_service(service)
+        session.close()
+        return response
 
+    session.close()
     return "ERROR"
 
 @app.route('/<id>/owner', methods=['POST'])
@@ -65,29 +71,39 @@ def create_owner_api(id)->str:
 
     body = request.json
 
+    session = Session()
     service = get_service(session, id)
     if service is not None:
         owner = create_owner(session, service, body['name'], body['information'],None,datetime.datetime.now())
         owner.url="/service/%d/owner/%d" % (service.id,owner.id)
         session.commit()
+        response = owner.get_dict()
+        session.close()
+        return json.dumps(response)
 
-        return json.dumps(owner.get_dict())
-
+    session.close()
     return "ERROR"
 
 
 
 @app.route('/<id>/owner/<id_owner>', methods=['GET', 'DELETE'])
 def get_owner_api(id, id_owner)->str:
+
+    session = Session()
     service = get_service(session, id)
     owner = get_owner(session, id_owner)
 
     if request.method == 'GET':
         if (service is not None) and (owner is not None) and (owner in service.owner):
-            return get_json_owner(owner)
+            response = get_json_owner(owner)
+            session.close()
+            return response
+
+        session.close()
         return "ERROR"
 
     delete_owner(session,owner=owner)
+    session.close()
     return "DELETED"
 
 
@@ -98,6 +114,7 @@ def create_domain_api(id, id_owner)->str:
 
     body = request.json
 
+    session = Session()
     service = get_service(session,id)
     owner = get_owner(session, id_owner)
     if (service is not None) and (owner is not None) and (owner in service.owner):
@@ -107,13 +124,17 @@ def create_domain_api(id, id_owner)->str:
 
             domain.url = "/service/%d/owner/%d/domain/%d" % (service.id, owner.id, domain.id)
             session.commit()
-            return json.dumps(domain.get_dict())
+            response = domain.get_dict()
+            session.close()
+            return json.dumps(response)
 
+    session.close()
     return "ERROR"
 
 @app.route('/<id>/owner/<id_owner>/domain/<id_domain>', methods=['GET', 'DELETE'])
 def get_domain_api(id, id_owner, id_domain)->str:
 
+    session = Session()
     service = get_service(session, id)
     owner   = get_owner(session, id_owner)
     domain  = get_domain(session, id_domain)
@@ -123,11 +144,15 @@ def get_domain_api(id, id_owner, id_domain)->str:
             and (domain is not None) and (owner in service.owner) \
                 and (domain in owner.domain):
 
-            return get_json_domain(domain)
+            response = get_json_domain(domain)
+            session.close()
+            return response
 
+        session.close()
         return "ERROR"
 
     delete_domain(session,domain=domain)
+    session.close()
     return "DELETED"
 
 
@@ -137,6 +162,7 @@ def create_element_api(id, id_owner, id_domain)->str:
 
     body = request.json
 
+    session = Session()
     service = get_service(session,id)
     owner = get_owner(session, id_owner)
     domain = get_domain(session, id_domain)
@@ -152,14 +178,18 @@ def create_element_api(id, id_owner, id_domain)->str:
         element.url = "/service/%d/owner/%d/domain/%d/element/%d" % (service.id, owner.id, domain.id, element.id)
         element.reserved = False
         session.commit()
-        return json.dumps(element.get_dict())
+        response = element.get_dict()
+        session.close()
+        return json.dumps(response)
 
+    session.close()
     return "ERROR"
 
 @app.route('/<id>/owner/<id_owner>/domain/<id_domain>/element/<id_element>',
            methods=['GET', 'DELETE'])
 def get_element_api(id, id_owner, id_domain, id_element)->str:
 
+    session = Session()
     service = get_service(session, id)
     owner   = get_owner(session, id_owner)
     domain  = get_domain(session, id_domain)
@@ -171,17 +201,23 @@ def get_element_api(id, id_owner, id_domain, id_element)->str:
              and (owner in service.owner) and (domain in owner.domain)\
               and (element in domain.element):
 
-            return json.dumps(element.get_dict())
+              response = element.get_dict()
+              session.close()
+              return json.dumps(response)
 
+        session.close()
         return "ERROR"
 
     delete_element(session, element=element)
+    session.close()
     return "DELETED"
 
 
 
 @app.route('/<id>/element/<id_element>', methods=['GET', 'DELETE', 'POST'])
 def get_element_byid_api(id, id_element):
+
+    session = Session()
     service = get_service(session, id)
     element = get_element(session, id_element)
 
@@ -192,7 +228,9 @@ def get_element_byid_api(id, id_element):
     if (service is not None) and (element is not None):
         #Get Reservation
         if request.method == 'GET':
-            return json.dumps(element.get_dict())
+            response = element.get_dict()
+            session.close()
+            return json.dumps(response)
 
         #Make Reservation
         elif request.method == 'POST' and (client is not None):
@@ -209,27 +247,35 @@ def get_element_byid_api(id, id_element):
             if reservation is not None:
                 reservation.url = "/service/%d/client/%d/reservation/%d" % (service.id, client.id, reservation.id)
                 session.commit()
-
-                return jsonify(reservation.get_dict())
+                response = reservation.get_dict()
+                session.close()
+                return jsonify(response)
 
             else:
+                session.close()
                 return "RESERVED"
 
         #Delete element
         delete_element(session, element=element)
+        session.close()
         return "DELETED"
 
+    session.close()
     return "ERROR"
 
 
 @app.route('/<id>/domain/<id_domain>', methods=['GET', 'DELETE', 'POST'])
 def get_domain_byid_api(id, id_domain)->str:
+
+    session = Session()
     service = get_service(session, id)
     domain = get_domain(session, id_domain)
 
     if(service is not None) and (domain is not None):
         if request.method == 'GET':
-            return get_json_domain(domain)
+            response = get_json_domain(domain)
+            session.close()
+            return response
 
         #Reserve one of the available elements from domain
         elif request.method == 'POST':
@@ -242,11 +288,13 @@ def get_domain_byid_api(id, id_domain)->str:
                 #Get Client
                 client = get_client(session, body["client"])
                 if client is None:
+                    session.close()
                     return "ERROR CLIENT"
 
                 #Get Element
                 element = get_element(session, elems[0])
                 if element is None:
+                    session.close()
                     return "ERROR ELEMENT"
 
                 if isinstance(body["information"], dict):
@@ -264,44 +312,54 @@ def get_domain_byid_api(id, id_domain)->str:
                     response            = reservation.get_dict()
                     response["price"]   = element.price
 
+                    session.close()
                     return jsonify(response)
 
                 else:
+                    session.close()
                     return "ALREADY RESERVED"
 
             else:
+                session.close()
                 return "ERROR"
 
         #delete domain
         delete_domain(session, domain=domain)
+        session.close()
         return "DELETED"
 
 
-
+    session.close()
     return "ERROR"
 
 @app.route('/<id>/domain/<id_domain>/get_aval_elems', methods=['GET'])
 def get_aval_elems(id, id_domain):
+    session = Session()
     service = get_service(session, id)
     domain = get_domain(session, id_domain)
 
     if(service is not None) and (domain is not None):
 
         elements = get_domain_aval_elements_w_info(session, domain=domain)
+        session.close()
         return jsonify(elements)
 
+    session.close()
     return "ERROR"
 
 @app.route('/<id>/domain/<id_domain>/get_dom_reservations', methods=['GET'])
 def get_domain_reservations_api(id, id_domain):
+    session = Session()
     service = get_service(session, id)
     domain = get_domain(session, id_domain)
 
     if (service is not None) and (domain is not None):
 
         reservations = get_domain_reservations(session, domain=domain)
+        session.close()
         return jsonify(reservations)
 
+    session.close()
     return "ERROR"
 
 
@@ -310,18 +368,23 @@ def create_client_api(id)->str:
 
     body = request.json
 
+    session = Session()
     service = get_service(session, id)
     if service is not None:
         client = create_client(session, service, body['name'], body['information'],None,datetime.datetime.now())
         client.url="/service/%d/client/%d" % (service.id,client.id)
         session.commit()
-        return json.dumps(client.get_dict())
+        response = client.get_dict()
+        session.close()
+        return json.dumps(response)
 
+    session.close()
     return "ERROR"
 
 @app.route('/<id>/client/<id_client>', methods=['GET', 'DELETE'])
 def get_client_api(id, id_client)->str:
 
+    session = Session()
     service = get_service(session, id)
     client = get_client(session, id_client)
 
@@ -329,10 +392,15 @@ def get_client_api(id, id_client)->str:
         if (service is not None) and (client is not None)\
             and (client in service.client):
 
-            return get_json_client(client)
+            response = get_json_client(client)
+            session.close()
+            return response
 
+        session.close()
         return "ERROR"
+
     delete_client(session, client=client)
+    session.close()
     return "DELETED"
 
 @app.route('/<id>/owner/<id_owner>/domain/<id_domain>/element/<id_element>',
@@ -340,6 +408,8 @@ def get_client_api(id, id_client)->str:
 def create_reservation_api(id, id_owner, id_domain, id_element)->str:
 
     body    = request.json
+
+    session = Session()
     service = get_service(session,id)
     owner   = get_owner(session, id_owner)
     domain  = get_domain(session, id_domain)
@@ -347,6 +417,7 @@ def create_reservation_api(id, id_owner, id_domain, id_element)->str:
     client  = get_client(session, body["id_client"])
 
     if (element is not None) and (element.reserved == True):
+        session.close()
         return "RESERVED"
 
     if (service is not None) and (owner is not None) and (domain is not None)\
@@ -361,13 +432,17 @@ def create_reservation_api(id, id_owner, id_domain, id_element)->str:
          reservation.url = "/service/%d/client/%d/reservation/%d" % (service.id, client.id, reservation.id)
          session.commit()
 
-         return json.dumps(reservation.get_dict())
+         response = reservation.get_dict()
+         session.close()
+         return json.dumps(response)
 
+    session.close()
     return "ERROR"
 
 @app.route('/<id>/client/<id_client>/reservation/<id_reservation>', methods=['GET', 'DELETE', 'PUT'])
 def get_reservation_api(id, id_client, id_reservation)->str:
 
+    session     = Session()
     service     = get_service(session, id)
     client      = get_client(session, id_client)
     reservation = get_reservation(session, id_reservation)
@@ -378,7 +453,9 @@ def get_reservation_api(id, id_client, id_reservation)->str:
 
             #Get Reservation
             if request.method == 'GET':
-                return json.dumps(reservation.get_dict())
+                response = reservation.get_dict()
+                session.close()
+                return json.dumps(response)
 
             #Update Information
             elif request.method == 'PUT':
@@ -391,13 +468,17 @@ def get_reservation_api(id, id_client, id_reservation)->str:
 
                     reservation.information = body["information"]
                     session.commit()
-                    
-                    return jsonify(reservation.get_dict())
+                    response = reservation.get_dict()
+                    session.close()
+
+                    return jsonify(response)
 
             #Delete reservation
             delete_reservation(session, reservation=reservation)
+            session.close()
             return "DELETED"
 
+    session.close()
     return "ERROR"
 
 
